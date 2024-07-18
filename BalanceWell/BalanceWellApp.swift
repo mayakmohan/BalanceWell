9fb4551ad177f564825661d37f6ae2e4cf01baa0
Maya Mohan <mohanmayak@gmail.com>
2024-07-18 13:17:17 -0700

    Initial Commit

---
+++ BalanceWell/BalanceWellApp.swift
@@ -0,0 +1,17 @@
+//
+//  BalanceWellApp.swift
+//  BalanceWell
+//
+//  Created by Smita Mohan on 7/18/24.
+//
+
+import SwiftUI
+
+@main
+struct BalanceWellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
